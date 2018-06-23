9b490acb99e07eff15d9e498a3765aa62c9c6e00
Henrique Troyack <henriquetroyack@corpugbvr.ugb.edu.br>
2018-06-23 16:50:43 -0300

    Adds test project created as of tutorialspoint.com ASP.NET MVC Tutorial

---
+++ asp.net_mvc/MVCFirstApp/MVCFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        /*
+        public ActionResult Index() {
+            return View();
+        }
+        */
+
+        public string Index()
+        {
+            var myst = new { controller = "Home", action = "Index", id = UrlParameter.Optional };
+
+            return "Hello World, this is ASP.Net MVC Tutorials";
+        }
+
+        public string Internautas()
+        {
+            return "Primeiro eu queria cumprimentar os internautas. -Oi Internautas";
+        }
+    }
+}
---
+++ asp.net_mvc/MVCFirstApp/MVCFirstApp/Controllers/InternautasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstApp.Controllers
+{
+    public class InternautasController : Controller
+    {
+        // GET: Internautas
+        public string Index()
+        {
+            return "dilma rousseff";
+        }
+        public string Internautas()
+        {
+            return "- Oi Internautas";
+        }
+    }
+}
